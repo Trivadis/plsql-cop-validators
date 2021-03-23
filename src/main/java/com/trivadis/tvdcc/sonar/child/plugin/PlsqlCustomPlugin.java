70502c8c674d20a305377d23216cafc3ab4803dc
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-03-23 20:32:05 +0100

    add PlsqlCustomPlugin class for all custom validators

---
+++ src/main/java/com/trivadis/tvdcc/sonar/child/plugin/PlsqlCustomPlugin.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
+ * 
+ * Licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 
+ * Unported License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ *     https://creativecommons.org/licenses/by-nc-nd/3.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.trivadis.tvdcc.sonar.child.plugin;
+
+import org.sonar.api.Plugin;
+
+public class PlsqlCustomPlugin implements Plugin {
+	public PlsqlCustomPlugin() {}
+	
+	@Override
+	public void define(Context context) {}
+
+}
