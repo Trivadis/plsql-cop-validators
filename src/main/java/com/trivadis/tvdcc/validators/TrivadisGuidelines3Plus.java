f23061a37c5aae63cd9458e7ad1aa02cbced48d4
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-12-04 16:32:30 +0100

    simplify TrivadisGuidelines3Plus.java

--- src/main/java/com/trivadis/tvdcc/validators/TrivadisGuidelines3Plus.java
+++ src/main/java/com/trivadis/tvdcc/validators/TrivadisGuidelines3Plus.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2020 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0
@@ -15,49 +15,46 @@
  */
 package com.trivadis.tvdcc.validators;
 
-import com.google.common.base.Objects;
 import com.trivadis.oracle.plsql.validation.PLSQLCopGuideline;
 import com.trivadis.oracle.plsql.validation.PLSQLCopValidator;
 import com.trivadis.oracle.plsql.validation.PLSQLValidator;
-import java.lang.annotation.Annotation;
-import java.util.Collection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.eclipse.xtext.validation.AbstractDeclarativeValidator;
 import org.eclipse.xtext.validation.ComposedChecks;
-import org.eclipse.xtext.xbase.lib.Conversions;
-import org.eclipse.xtext.xbase.lib.Exceptions;
-import org.eclipse.xtext.xbase.lib.Functions.Function1;
-import org.eclipse.xtext.xbase.lib.IterableExtensions;
-import org.eclipse.xtext.xbase.lib.ListExtensions;
 
 @ComposedChecks(validators = { OverrideTrivadisGuidelines.class, SQLInjection.class, TrivadisPlsqlNaming.class,
         Hint.class })
-@SuppressWarnings("all")
 public class TrivadisGuidelines3Plus extends PLSQLValidator implements PLSQLCopValidator {
     private final TrivadisGuidelines3 converter = new TrivadisGuidelines3();
-
     private HashMap<Integer, PLSQLCopGuideline> guidelines;
 
+    // TODO: remove when TrivadisGuidelines2 uses new IDs
     @Override
-    public String getGuidelineId(final Integer id) {
-        if (((id).intValue() < 1000)) {
-            return String.format("G-%04d", this.converter.getNewId(id));
+    public String getGuidelineId(Integer id) {
+        if (id < 1000) {
+            return String.format("G-%04d", converter.getNewId(id));
         } else {
             return super.getGuidelineId(id);
         }
     }
 
+    // TODO: remove when TrivadisGuidelines2 uses new IDs
     @Override
-    public String getGuidelineMsg(final Integer id) {
-        if (((id).intValue() < 1000)) {
-            final Integer newId = this.converter.getNewId(id);
-            final PLSQLCopGuideline guideline = this.getGuidelines().get(newId);
-            String _msg = null;
+    public String getGuidelineMsg(Integer id) {
+        if (id < 1000) {
+            final Integer newId = converter.getNewId(id);
+            final PLSQLCopGuideline guideline = getGuidelines().get(newId);
+            String msg = null;
             if (guideline != null) {
-                _msg = guideline.getMsg();
+                msg = guideline.getMsg();
             }
-            return String.format("G-%04d: %s", newId, _msg);
+            return String.format("G-%04d: %s", newId, msg);
         } else {
             return super.getGuidelineMsg(id);
         }
@@ -65,45 +62,33 @@ public class TrivadisGuidelines3Plus extends PLSQLValidator implements PLSQLCopV
 
     @Override
     public HashMap<Integer, PLSQLCopGuideline> getGuidelines() {
-        try {
-            if ((this.guidelines == null)) {
-                HashMap<Integer, PLSQLCopGuideline> _hashMap = new HashMap<Integer, PLSQLCopGuideline>();
-                this.guidelines = _hashMap;
-                final Function1<Annotation, Boolean> _function = (Annotation it) -> {
-                    Class<? extends Annotation> _annotationType = it.annotationType();
-                    return Boolean.valueOf(Objects.equal(_annotationType, ComposedChecks.class));
-                };
-                final Function1<Annotation, ComposedChecks> _function_1 = (Annotation it) -> {
-                    return ((ComposedChecks) it);
-                };
-                final List<ComposedChecks> annotations = ListExtensions.<Annotation, ComposedChecks>map(
-                        IterableExtensions.<Annotation>toList(IterableExtensions.<Annotation>filter(
-                                ((Iterable<Annotation>) Conversions.doWrapArray(this.getClass().getAnnotations())),
-                                _function)),
-                        _function_1);
-                for (final ComposedChecks annotation : annotations) {
-                    Class<? extends AbstractDeclarativeValidator>[] _validators = annotation.validators();
-                    for (final Class<? extends AbstractDeclarativeValidator> validator : _validators) {
-                        {
-                            final AbstractDeclarativeValidator validatorInstance = validator.newInstance();
-                            Collection<PLSQLCopGuideline> _values = ((PLSQLCopValidator) validatorInstance)
-                                    .getGuidelines().values();
-                            for (final PLSQLCopGuideline guideline : _values) {
-                                Integer _id = guideline.getId();
-                                boolean _lessThan = ((_id).intValue() < 1000);
-                                if (_lessThan) {
-                                    this.guidelines.put(this.converter.getNewId(guideline.getId()), guideline);
-                                } else {
-                                    this.guidelines.put(guideline.getId(), guideline);
-                                }
-                            }
+        if (guidelines == null) {
+            guidelines = new HashMap<>();
+            final List<ComposedChecks> annotations = Arrays.stream(getClass().getAnnotations())
+                    .filter(it -> it.annotationType() == ComposedChecks.class).collect(Collectors.toList()).stream()
+                    .map(it -> (ComposedChecks) it).collect(Collectors.toList());
+            for (final ComposedChecks annotation : annotations) {
+                Class<? extends AbstractDeclarativeValidator>[] _validators = annotation.validators();
+                for (final Class<? extends AbstractDeclarativeValidator> validator : _validators) {
+                    final AbstractDeclarativeValidator validatorInstance;
+                    try {
+                        validatorInstance = validator.getDeclaredConstructor().newInstance();
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException
+                            | NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                    for (final PLSQLCopGuideline guideline : ((PLSQLCopValidator) validatorInstance).getGuidelines()
+                            .values()) {
+                        if (guideline.getId() < 1000) {
+                            // TODO: remove when TrivadisGuidelines2 uses new IDs
+                            guidelines.put(converter.getNewId(guideline.getId()), guideline);
+                        } else {
+                            guidelines.put(guideline.getId(), guideline);
                         }
                     }
                 }
             }
-            return this.guidelines;
-        } catch (Throwable _e) {
-            throw Exceptions.sneakyThrow(_e);
         }
+        return guidelines;
     }
 }
