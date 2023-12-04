61670a008d3fce383514f48a80aebd8af5dff5a9
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-12-04 15:48:26 +0100

    format TrivadisGuidelines3Plus.java

--- src/main/java/com/trivadis/tvdcc/validators/TrivadisGuidelines3Plus.java
+++ src/main/java/com/trivadis/tvdcc/validators/TrivadisGuidelines3Plus.java
@@ -31,73 +31,79 @@ import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.ListExtensions;
 
-@ComposedChecks(validators = { OverrideTrivadisGuidelines.class, SQLInjection.class, TrivadisPlsqlNaming.class, Hint.class })
+@ComposedChecks(validators = { OverrideTrivadisGuidelines.class, SQLInjection.class, TrivadisPlsqlNaming.class,
+        Hint.class })
 @SuppressWarnings("all")
 public class TrivadisGuidelines3Plus extends PLSQLValidator implements PLSQLCopValidator {
-  private final TrivadisGuidelines3 converter = new TrivadisGuidelines3();
+    private final TrivadisGuidelines3 converter = new TrivadisGuidelines3();
 
-  private HashMap<Integer, PLSQLCopGuideline> guidelines;
+    private HashMap<Integer, PLSQLCopGuideline> guidelines;
 
-  @Override
-  public String getGuidelineId(final Integer id) {
-    if (((id).intValue() < 1000)) {
-      return String.format("G-%04d", this.converter.getNewId(id));
-    } else {
-      return super.getGuidelineId(id);
-    }
-  }
-
-  @Override
-  public String getGuidelineMsg(final Integer id) {
-    if (((id).intValue() < 1000)) {
-      final Integer newId = this.converter.getNewId(id);
-      final PLSQLCopGuideline guideline = this.getGuidelines().get(newId);
-      String _msg = null;
-      if (guideline!=null) {
-        _msg=guideline.getMsg();
-      }
-      return String.format("G-%04d: %s", newId, _msg);
-    } else {
-      return super.getGuidelineMsg(id);
-    }
-  }
-
-  @Override
-  public HashMap<Integer, PLSQLCopGuideline> getGuidelines() {
-    try {
-      if ((this.guidelines == null)) {
-        HashMap<Integer, PLSQLCopGuideline> _hashMap = new HashMap<Integer, PLSQLCopGuideline>();
-        this.guidelines = _hashMap;
-        final Function1<Annotation, Boolean> _function = (Annotation it) -> {
-          Class<? extends Annotation> _annotationType = it.annotationType();
-          return Boolean.valueOf(Objects.equal(_annotationType, ComposedChecks.class));
-        };
-        final Function1<Annotation, ComposedChecks> _function_1 = (Annotation it) -> {
-          return ((ComposedChecks) it);
-        };
-        final List<ComposedChecks> annotations = ListExtensions.<Annotation, ComposedChecks>map(IterableExtensions.<Annotation>toList(IterableExtensions.<Annotation>filter(((Iterable<Annotation>)Conversions.doWrapArray(this.getClass().getAnnotations())), _function)), _function_1);
-        for (final ComposedChecks annotation : annotations) {
-          Class<? extends AbstractDeclarativeValidator>[] _validators = annotation.validators();
-          for (final Class<? extends AbstractDeclarativeValidator> validator : _validators) {
-            {
-              final AbstractDeclarativeValidator validatorInstance = validator.newInstance();
-              Collection<PLSQLCopGuideline> _values = ((PLSQLCopValidator) validatorInstance).getGuidelines().values();
-              for (final PLSQLCopGuideline guideline : _values) {
-                Integer _id = guideline.getId();
-                boolean _lessThan = ((_id).intValue() < 1000);
-                if (_lessThan) {
-                  this.guidelines.put(this.converter.getNewId(guideline.getId()), guideline);
-                } else {
-                  this.guidelines.put(guideline.getId(), guideline);
-                }
-              }
-            }
-          }
+    @Override
+    public String getGuidelineId(final Integer id) {
+        if (((id).intValue() < 1000)) {
+            return String.format("G-%04d", this.converter.getNewId(id));
+        } else {
+            return super.getGuidelineId(id);
         }
-      }
-      return this.guidelines;
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
     }
-  }
+
+    @Override
+    public String getGuidelineMsg(final Integer id) {
+        if (((id).intValue() < 1000)) {
+            final Integer newId = this.converter.getNewId(id);
+            final PLSQLCopGuideline guideline = this.getGuidelines().get(newId);
+            String _msg = null;
+            if (guideline != null) {
+                _msg = guideline.getMsg();
+            }
+            return String.format("G-%04d: %s", newId, _msg);
+        } else {
+            return super.getGuidelineMsg(id);
+        }
+    }
+
+    @Override
+    public HashMap<Integer, PLSQLCopGuideline> getGuidelines() {
+        try {
+            if ((this.guidelines == null)) {
+                HashMap<Integer, PLSQLCopGuideline> _hashMap = new HashMap<Integer, PLSQLCopGuideline>();
+                this.guidelines = _hashMap;
+                final Function1<Annotation, Boolean> _function = (Annotation it) -> {
+                    Class<? extends Annotation> _annotationType = it.annotationType();
+                    return Boolean.valueOf(Objects.equal(_annotationType, ComposedChecks.class));
+                };
+                final Function1<Annotation, ComposedChecks> _function_1 = (Annotation it) -> {
+                    return ((ComposedChecks) it);
+                };
+                final List<ComposedChecks> annotations = ListExtensions.<Annotation, ComposedChecks>map(
+                        IterableExtensions.<Annotation>toList(IterableExtensions.<Annotation>filter(
+                                ((Iterable<Annotation>) Conversions.doWrapArray(this.getClass().getAnnotations())),
+                                _function)),
+                        _function_1);
+                for (final ComposedChecks annotation : annotations) {
+                    Class<? extends AbstractDeclarativeValidator>[] _validators = annotation.validators();
+                    for (final Class<? extends AbstractDeclarativeValidator> validator : _validators) {
+                        {
+                            final AbstractDeclarativeValidator validatorInstance = validator.newInstance();
+                            Collection<PLSQLCopGuideline> _values = ((PLSQLCopValidator) validatorInstance)
+                                    .getGuidelines().values();
+                            for (final PLSQLCopGuideline guideline : _values) {
+                                Integer _id = guideline.getId();
+                                boolean _lessThan = ((_id).intValue() < 1000);
+                                if (_lessThan) {
+                                    this.guidelines.put(this.converter.getNewId(guideline.getId()), guideline);
+                                } else {
+                                    this.guidelines.put(guideline.getId(), guideline);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return this.guidelines;
+        } catch (Throwable _e) {
+            throw Exceptions.sneakyThrow(_e);
+        }
+    }
 }
