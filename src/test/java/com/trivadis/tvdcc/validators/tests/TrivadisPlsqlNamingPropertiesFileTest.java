df7e0b227e2ad2f38fc03b78e8e567d1599ab2fc
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-12-12 16:21:38 +0100

    simplify TrivadisPlsqlNamingPropertiesFileTest.java

--- src/test/java/com/trivadis/tvdcc/validators/tests/TrivadisPlsqlNamingPropertiesFileTest.java
+++ src/test/java/com/trivadis/tvdcc/validators/tests/TrivadisPlsqlNamingPropertiesFileTest.java
@@ -1,143 +1,108 @@
+/*
+ * Copyright 2020 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
+ * 
+ * Licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0
+ * Unported License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ *     https://creativecommons.org/licenses/by-nc-nd/3.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.trivadis.tvdcc.validators.tests;
 
-import com.google.common.base.Objects;
 import com.trivadis.oracle.plsql.validation.PLSQLValidatorPreferences;
 import com.trivadis.tvdcc.validators.TrivadisPlsqlNaming;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.util.List;
-import org.eclipse.xtend2.lib.StringConcatenation;
-import org.eclipse.xtext.validation.Issue;
-import org.eclipse.xtext.xbase.lib.Exceptions;
-import org.eclipse.xtext.xbase.lib.Functions.Function1;
-import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import java.io.IOException;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-@SuppressWarnings("all")
 public class TrivadisPlsqlNamingPropertiesFileTest extends AbstractValidatorTest {
+
     @BeforeClass
     public static void commonSetup() {
-        AbstractValidatorTest.stashPropertiesFile();
-        TrivadisPlsqlNamingPropertiesFileTest.createTestPropertiesFile();
+        stashPropertiesFile();
+        createTestPropertiesFile();
         PLSQLValidatorPreferences.INSTANCE.setValidatorClass(TrivadisPlsqlNaming.class);
     }
 
+    // create a simple properties file to test with 
     public static void createTestPropertiesFile() {
         try {
-            final File file = new File(TrivadisPlsqlNamingTest.FULL_PROPERTIES_FILE_NAME);
+            final File file = new File(FULL_PROPERTIES_FILE_NAME);
             final FileWriter fileWriter = new FileWriter(file, true);
             final BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.write("PREFIX_LOCAL_VARIABLE_NAME = loc_");
             bufferedWriter.newLine();
             bufferedWriter.close();
             fileWriter.close();
-        } catch (Throwable _e) {
-            throw Exceptions.sneakyThrow(_e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
+    // check that old prefix is now not accepted
     @Test
     public void LocalVariableNok() {
-        StringConcatenation _builder = new StringConcatenation();
-        _builder.append("CREATE OR REPLACE PACKAGE BODY example AS");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("PROCEDURE a IS");
-        _builder.newLine();
-        _builder.append("      ");
-        _builder.append("l_some_name INTEGER;");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("BEGIN");
-        _builder.newLine();
-        _builder.append("      ");
-        _builder.append("NULL;");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("END a;");
-        _builder.newLine();
-        _builder.append("END example;");
-        _builder.newLine();
-        final String stmt = _builder.toString();
-        final List<Issue> issues = this.getIssues(stmt);
-        final Function1<Issue, Boolean> _function = (Issue it) -> {
-            String _code = it.getCode();
-            return Boolean.valueOf(Objects.equal(_code, "G-9102"));
-        };
-        Assert.assertEquals(1, IterableExtensions.size(IterableExtensions.<Issue>filter(issues, _function)));
+        var stmt = """
+                CREATE OR REPLACE PACKAGE BODY example AS
+                   PROCEDURE a IS
+                      l_some_name INTEGER;
+                   BEGIN
+                      NULL;
+                   END a;
+                END example;
+                """;
+        var issues = getIssues(stmt);
+        Assert.assertEquals(1, issues.stream().filter(it -> it.getCode().equals("G-9102")).toList().size());
     }
 
+    // check that new prefix from file is accepted
     @Test
     public void LocalVariableOk() {
-        StringConcatenation _builder = new StringConcatenation();
-        _builder.append("CREATE OR REPLACE PACKAGE BODY example AS");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("PROCEDURE a IS");
-        _builder.newLine();
-        _builder.append("      ");
-        _builder.append("loc_some_name INTEGER;");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("BEGIN");
-        _builder.newLine();
-        _builder.append("      ");
-        _builder.append("NULL;");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("END a;");
-        _builder.newLine();
-        _builder.append("END example;");
-        _builder.newLine();
-        final String stmt = _builder.toString();
-        final List<Issue> issues = this.getIssues(stmt);
-        final Function1<Issue, Boolean> _function = (Issue it) -> {
-            String _code = it.getCode();
-            return Boolean.valueOf(Objects.equal(_code, "G-9102"));
-        };
-        Assert.assertEquals(0, IterableExtensions.size(IterableExtensions.<Issue>filter(issues, _function)));
+        var stmt = """
+                CREATE OR REPLACE PACKAGE BODY example AS
+                   PROCEDURE a IS
+                      loc_some_name INTEGER;
+                   BEGIN
+                      NULL;
+                   END a;
+                END example;
+                """;
+        var issues = getIssues(stmt);
+        Assert.assertEquals(0, issues.stream().filter(it -> it.getCode().equals("G-9102")).toList().size());
     }
 
+    // check that defaults are used if not specified in the properties-file
     @Test
     public void GlobalVariableNok() {
-        StringConcatenation _builder = new StringConcatenation();
-        _builder.append("CREATE OR REPLACE PACKAGE example AS");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("some_name INTEGER;");
-        _builder.newLine();
-        _builder.append("END example;");
-        _builder.newLine();
-        _builder.append("/");
-        _builder.newLine();
-        final String stmt = _builder.toString();
-        final List<Issue> issues = this.getIssues(stmt);
-        final Function1<Issue, Boolean> _function = (Issue it) -> {
-            String _code = it.getCode();
-            return Boolean.valueOf(Objects.equal(_code, "G-9101"));
-        };
-        Assert.assertEquals(1, IterableExtensions.size(IterableExtensions.<Issue>filter(issues, _function)));
+        var stmt = """
+                CREATE OR REPLACE PACKAGE example AS
+                   some_name INTEGER;
+                END example;
+                /
+                """;
+        var issues = getIssues(stmt);
+        Assert.assertEquals(1, issues.stream().filter(it -> it.getCode().equals("G-9101")).toList().size());
     }
 
+    // check that defaults are used if not specified in the properties-file
     @Test
     public void GlobalVariableOk() {
-        StringConcatenation _builder = new StringConcatenation();
-        _builder.append("CREATE OR REPLACE PACKAGE example AS");
-        _builder.newLine();
-        _builder.append("   ");
-        _builder.append("g_some_name INTEGER;");
-        _builder.newLine();
-        _builder.append("END example;");
-        _builder.newLine();
-        final String stmt = _builder.toString();
-        final List<Issue> issues = this.getIssues(stmt);
-        final Function1<Issue, Boolean> _function = (Issue it) -> {
-            String _code = it.getCode();
-            return Boolean.valueOf(Objects.equal(_code, "G-9101"));
-        };
-        Assert.assertEquals(0, IterableExtensions.size(IterableExtensions.<Issue>filter(issues, _function)));
+        var stmt = """
+                CREATE OR REPLACE PACKAGE example AS
+                   g_some_name INTEGER;
+                END example;
+                """;
+        var issues = getIssues(stmt);
+        Assert.assertEquals(0, issues.stream().filter(it -> it.getCode().equals("G-9101")).toList().size());
     }
 }
