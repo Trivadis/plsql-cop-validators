c22ad298419a5036bb44791b13d27e3e503d9939
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2024-03-19 15:25:21 +0100

    update link to guidelines v4.4

--- src/main/resources/TrivadisGuidelines3Plus/sample/guideline_9104.sql
+++ src/main/resources/TrivadisGuidelines3Plus/sample/guideline_9104.sql
@@ -1,7 +1,7 @@
 -- G-9104: Always name records to match '^r_.+$'.
 
 -- Reason
-/*<p>See <a href="https://trivadis.github.io/plsql-and-sql-coding-guidelines/v4.3/2-naming-conventions/naming-conventions/#naming-conventions-for-plsql" target="_blank">Naming Conventions for PL/SQL</a>.</p>
+/*<p>See <a href="https://trivadis.github.io/plsql-and-sql-coding-guidelines/v4.4/2-naming-conventions/naming-conventions/#naming-conventions-for-plsql" target="_blank">Naming Conventions for PL/SQL</a>.</p>
 <p>You can override the default via system property REGEX_RECORD_NAME.</p>*/
 
 -- Bad
